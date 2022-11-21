4a845c603af20b9ebb1c9b72fbef9567ee7cca87
Maxim Sidorov <s_maxim_v@bk.ru>
2022-11-21 22:23:16 +0400

    init xcodeproject

---
+++ CachingPlayerSample/CachingPlayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingPlayerSampleApp.swift
+//  CachingPlayerSample
+//
+//  Created by MSP on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CachingPlayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
